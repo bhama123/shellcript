83a83fa6bc274d2572bd61a8bfd6daba39856a7c
bhama123 <nsbhama7@gmail.com>
2025-03-28 12:29:28 +0530

    Add files via upload

---
+++ smallest.sh
@@ -0,0 +1,10 @@
+echo "enter two numbers"
+read a b
+if [$b le $a ] ; then
+echo " smallest is $b"
+else
+echo " smallest is $a"
+fi
+enter two number
+2  5
+smallest is 2
---
+++ sum.sh
@@ -0,0 +1,7 @@
+echo " enter two number"
+read a b
+c=`expr $a + $b`
+echo  "sum is" $c
+ enter two numbers
+3 4
+sum is 7
